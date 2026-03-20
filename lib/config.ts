1bfa1a9725b3a089ff66ad2b75d5fab1491a0330
RusakRule1 <rusak.ant@gmail.com>
2026-03-20 17:34:29 +0100

    feat: add centralized site config

---
+++ lib/config.ts
@@ -0,0 +1,13 @@
+export const SITE_CONFIG = {
+  name: "Antonio Rusak",
+  role: "Software Developer",
+  email: "rusak.ant@gmail.com",
+  github: {
+    username: "RusakRule1",
+    url: "https://github.com/RusakRule1",
+  },
+  linkedin: {
+    url: "https://www.linkedin.com/in/antonio-r-5b3705335",
+    label: "linkedin.com/in/antonio-r",
+  },
+} as const;
